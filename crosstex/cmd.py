97fb35221669876da10942c5716b35bd35821a7a
Robert Escriva <me@robescriva.com>
2012-09-14 16:47:29 -0400

    Fix an assertion error

--- crosstex/cmd.py
+++ crosstex/cmd.py
@@ -130,9 +130,8 @@ parser.add_argument('files', metavar='FILES', nargs='+',
 
 def main(argv):
     args = parser.parse_args()
-    assert args.dirs
     db = crosstex.Database()
-    for path in args.dirs:
+    for path in args.dirs or []:
         db.append_path(path)
     db.append_path(os.path.join(os.path.join(os.path.expanduser('~'), '.crosstex')))
     db.append_path('/XXX') # XXX system crosstex
