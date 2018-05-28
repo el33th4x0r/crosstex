e59daa4c9bc70bf6e5bce3478373d4ad9b1e11d0
Kai Mast <kaimast@cs.cornell.edu>
2018-05-28 19:00:04 -0400

    Install latex base

--- run-tests.py
+++ run-tests.py
@@ -18,7 +18,7 @@ def run_test(path, filename):
     print("### Running " + path)
 
     # Prepare doc
-    latex_res = call(["xelatex", "--halt-on-error", filename], cwd=DIR, stdout=DEVNULL)
+    latex_res = call(["xelatex", "-interaction", "errorstopmode", filename], cwd=DIR, stdout=DEVNULL)
 
     if latex_res != 0:
         print("WARNING: xelatex failed")
